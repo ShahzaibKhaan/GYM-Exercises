8175b2129c277891e99819565bfdd6698e8a366d
ShahzaibKhaan <shahzenkhaan@gmail.com>
2025-12-08 18:30:16 +0500

    full final by Shahzaib

---
+++ App.js
@@ -0,0 +1,32 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './main-1/home';
+import DetailScreen from './main-1/detailed';
+import AddExerciseScreen from './main-1/add';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
+        <Stack.Screen name="AddExerciseScreen" component={AddExerciseScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ main-1/add.js
@@ -0,0 +1,50 @@
+import { View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+
+export default function AddExerciseScreen({ navigation, route }) {
+
+  const { addExercise } = route.params;
+
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [image, setImage] = useState('');
+
+  const handleAdd = () => {
+    const newExercise = {
+      id: Date.now().toString(),
+      title: title,
+      description: desc,
+      image: image,
+    };
+
+    addExercise(newExercise);   
+    navigation.goBack();        
+  };
+
+  return (
+    <View style={{ padding: 20 }}>
+      <TextInput
+        placeholder="Exercise Name"
+        value={title}
+        onChangeText={setTitle}
+        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
+      />
+
+      <TextInput
+        placeholder="Description"
+        value={desc}
+        onChangeText={setDesc}
+        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
+      />
+
+      <TextInput
+        placeholder="Image URL"
+        value={image}
+        onChangeText={setImage}
+        style={{ borderWidth: 1, padding: 10, marginBottom: 20 }}
+      />
+
+      <Button title="Add Exercise" onPress={handleAdd} />
+    </View>
+  );
+}
